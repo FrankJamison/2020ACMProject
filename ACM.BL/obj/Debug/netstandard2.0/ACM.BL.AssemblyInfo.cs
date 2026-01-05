3ee26b36bd501ee180fffd890ecac928c592a155
FrankJamison <frank@frankjamison.com>
2026-01-05 11:29:59 -0800

    updates to run in vscode

--- ACM.BL/obj/Debug/netstandard2.0/ACM.BL.AssemblyInfo.cs
+++ ACM.BL/obj/Debug/netstandard2.0/ACM.BL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ACM.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52612614ed9281221ed76a556023a053ca1e0c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3231c8bed49684c8b86093b7af4704ea45a8e225")]
 [assembly: System.Reflection.AssemblyProductAttribute("ACM.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ACM.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
